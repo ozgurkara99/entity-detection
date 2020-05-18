894d18f7ea7b2112142b333ddcbd5916465009ab
Özgür Kara <49339814+ozgurkara99@users.noreply.github.com>
2020-05-18 19:51:27 +0300

    Update tx_positions.m

--- index_modulation/matlab_code/tx_positions.m
+++ index_modulation/matlab_code/tx_positions.m
@@ -1,4 +1,4 @@
-function [arr] = tx_positions(center_of_UCA,num_of_tx)
+function [arr] = tx_positions(center_of_UCA,num_of_tx, d_yz, r_tx)
 %{
     """Create the transmitter's coordinates in a circular region and distributed 
     uniformly in angle depending on the self.num_of_tx
@@ -11,8 +11,8 @@ function [arr] = tx_positions(center_of_UCA,num_of_tx)
 
     d = transpose(linspace(0,1,(num_of_tx)));
     theta = d * 2 * pi;
-    y = center_of_UCA(2) + 4 * cos(theta);
-    z = center_of_UCA(3) + 4 * sin(theta);
+    y = center_of_UCA(2) + (d_yz + r_tx) * cos(theta);
+    z = center_of_UCA(3) + (d_yz + r_tx) * sin(theta);
     x = ones(num_of_tx,1) * center_of_UCA(1);
     arr = [x y z];
 end
