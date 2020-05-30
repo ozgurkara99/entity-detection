a6732a74f07b362c9f9c1bdec515c3c1f52316cb
Özgür Kara <49339814+ozgurkara99@users.noreply.github.com>
2020-05-30 14:50:04 +0300

    classes and output of multiple time series

--- index_modulation/preprocess.py
+++ index_modulation/preprocess.py
@@ -1,24 +1,24 @@
 import numpy as np
 import csv
 import math
+
 PI = math.pi
 time = 0.75
-downsample = 0.01
-size = time / downsample
+downsample = 0.001
+az_pair, el_pair = 4, 4
 filepath = 'data\\output.csv'
+size = time / downsample
 
 """
 1 -> theta yani azimuth
 2 -> phi yani elevation
 """
-az_pair, el_pair = 4, 4
+
 def az_el_pair(az_num, el_num):
     azimuth = np.linspace(-PI, PI, az_num + 1)
     elevation = np.linspace(0,PI, el_num + 1)
     return azimuth, elevation
 
-def preprocess(arr):
-    pass
 def read_data(filepath):
     tri = []
     temp = []
@@ -31,7 +31,7 @@ def read_data(filepath):
                 tri.append(np.array(temp,dtype=float))
                 temp = []
             i=i+1
-            if(i==31):
+            if(i==601):
                 break
     return tri
 
@@ -40,7 +40,7 @@ tri = read_data(filepath)
 
 output = np.zeros((len(tri),az_pair * el_pair + 1,int(size)))
 output[:,az_pair * el_pair,:] = np.linspace(0,time - downsample,int(size))
-
+classes = np.zeros((len(tri),1))
 az_list, el_list = az_el_pair(az_pair, el_pair)
 for inn,lis in enumerate(tri):
     for time in range(int(size)):    
@@ -52,3 +52,4 @@ for inn,lis in enumerate(tri):
                     if((az_list[j] <= az[0,i] < az_list[j+1]) & (el_list[k] <= el[0,i] < el_list[k+1])):
                         index = j * az_pair + k
                         output[inn,index,time] = output[inn,index,time] + 1
+                        classes[inn,0] = lis[0,-1] 
